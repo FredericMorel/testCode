71617955dccaa3e5dc8d1a4571dda9af3f115800
Freddy4773 <39941348+FredericMorel@users.noreply.github.com>
2021-02-26 09:48:59 +0400

    ajout fichiers de base

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apprendre le HTML</title>
+</head>
+
+<body>
+    <header>
+        <hgroup>
+            <h1 title="Le titre est en latin. Merde!">Calculus I</h1>
+            <h2>Fundamentals</h2>
+        </hgroup>
+        <nav>
+            <h1>Ma Barre de menu</h1>
+            <ul>
+            </ul><a href=""></a>
+            <a href=""></a>
+        </nav>
+    </header>
+    <section>
+        <h1></h1>
+        <section>
+            <h1></h1>
+            <p>The three primary web development technologies are
+                <abbr title="Hypertext Markup Language">HTML</abbr>,
+                <abbr title="Cascading Stylesheets">CSS</abbr>, and
+                JavaScript.
+            </p>
+
+        </section>
+    </section>
+
+    <footer></footer>
+</body>
+
+</html>
+
+
+
+<?php
+$dir = dirname("/etc/passwd") . PHP_EOL; ?>
+
+
+
+<?php echo $_SERVER['DOCUMENT_ROOT']; ?>
